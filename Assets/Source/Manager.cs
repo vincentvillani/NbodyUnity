56f8b544585df15604c64a5b0839a7bede90915c
Vincent Villani <villanivincent@gmail.com>
2016-06-30 02:57:47 +1000

    Initial commit

---
+++ Assets/Source/Manager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour 
+{
+    const float POSITIONVALUE = 1000.0f;
+
+    const float VELOCITYVALUE = 0.5f;
+
+    const float MINVALUEMASS = 0.1f;
+    const float MAXVALUEMASS = 100.0f;
+
+    const int MASS_NUMBER = 300;
+
+    List<Mass> massList;
+
+
+	void Start () 
+    {
+        massList = new List<Mass>();
+
+        for(int i = 0; i < MASS_NUMBER; ++i)
+        {
+            massList.Add(new Mass(i,
+                Random.Range(MINVALUEMASS, MAXVALUEMASS),
+                new Vector3(Random.Range(-VELOCITYVALUE, VELOCITYVALUE), Random.Range(-VELOCITYVALUE, VELOCITYVALUE), Random.Range(-VELOCITYVALUE, VELOCITYVALUE)),
+                new Vector3(Random.Range(-POSITIONVALUE, POSITIONVALUE), Random.Range(-POSITIONVALUE, POSITIONVALUE), Random.Range(-POSITIONVALUE, POSITIONVALUE))));
+        }
+	}
+	
+
+
+
+	void Update () 
+    {
+        //Update force interactions
+	    for(int i = 0; i < MASS_NUMBER; ++i)
+        {
+            for(int j = i + 1; j < MASS_NUMBER; ++j)
+            {
+                CalculateForceInteraction(massList[i], massList[j]);
+            }
+        }
+
+        //Update the positions
+        for(int i = 0; i < MASS_NUMBER; ++i)
+        {
+            massList[i].gameobject.transform.position += massList[i].velocity * Time.deltaTime;
+        }
+
+    }
+
+
+    //6.674×10^−11
+    public void CalculateForceInteraction(Mass mass1, Mass mass2)
+    {
+        //const float gravitionalConstant = 0.00000000006674f;
+
+        //Vector is from mass1 to mass2
+        Vector3 distanceVector = mass2.gameobject.transform.position - mass1.gameobject.transform.position;
+        float distanceSquared = distanceVector.sqrMagnitude;
+        Vector3 distanceVectorNormalised = distanceVector.normalized;
+
+        float force = ((mass1.mass * mass2.mass) / distanceSquared); //gravitionalConstant * ( (mass1.mass * mass2.mass) / distanceSquared);
+
+        Vector3 forceVector = force * distanceVectorNormalised * Time.deltaTime;
+        mass1.velocity += forceVector;
+        mass2.velocity -= forceVector;
+
+    }
+}
---
+++ Assets/Source/Mass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Mass 
+{
+    public int id;
+    public float mass;
+    public Vector3 velocity;
+    public GameObject gameobject;
+
+    public Mass(int id, float mass, Vector3 velocity, Vector3 position)
+    {
+        this.id = id;
+        this.mass = mass;
+        this.velocity = velocity;
+
+
+        gameobject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        gameobject.transform.position = position;
+        gameobject.transform.localScale = new Vector3(mass, mass, mass);
+        GameObject.Destroy(gameobject.GetComponent<SphereCollider>());
+    }
+
+}
